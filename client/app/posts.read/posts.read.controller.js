7855f60aa390e8b90c2db58f80d8ad6f271c6087
Braun Goodson <braungoodson@gmail.com>
2015-02-22 14:11:07 -0500

    defined controller; defined view; defined footer;

--- client/app/posts.read/posts.read.controller.js
+++ client/app/posts.read/posts.read.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('blogApp')
-  .controller('PostsReadCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PostsReadCtrl', function ($scope, $http, socket) {
+    $scope.posts = [];
+    $http.get('/api/posts')
+      .success(function(posts){
+        $scope.posts = posts;
+        socket.syncUpdates('post',$scope.posts);
+      })
+      .error(function(error){
+        $scope.error = error;
+        throw new Error(error);
+      })
+    ;
+    $scope.$on('$destroy',function(){
+      socket.unsyncUpdates('post');
+    });
   });
