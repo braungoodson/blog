9ff244a18f8e33a6ae8713fb1666ce37519dc5d6
Braun Goodson <braungoodson@gmail.com>
2015-02-23 22:27:14 -0500

    fixed post create redirect

--- client/app/posts.create/posts.create.controller.js
+++ client/app/posts.create/posts.create.controller.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('blogApp')
-  .controller('PostsCreateCtrl', function ($scope, $http, Auth) {
+  .controller('PostsCreateCtrl', function ($scope, $http, Auth, $state) {
     $scope.post = {};
     $scope.create = function() {
       var error = 'You must be logged in to do that.';
       $scope.post.author = Auth.getCurrentUser()._id;
-      $http.post('/api/posts',$scope.post);
+      $http.post('/api/posts',$scope.post)
+        .success(function(post){
+          $state.go('posts.read');
+        })
+        .error(function(error){
+          $scope.error = error;
+          throw new Error(error);
+        })
+      ;
     };
   });
