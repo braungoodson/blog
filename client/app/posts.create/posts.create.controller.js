abeee422e0c4df0740e533f492b4c746c83e53a7
Braun Goodson <braungoodson@gmail.com>
2015-02-24 19:54:52 -0500

    fixed prebuild syntax errors

--- client/app/posts.create/posts.create.controller.js
+++ client/app/posts.create/posts.create.controller.js
@@ -4,11 +4,11 @@ angular.module('blogApp')
   .controller('PostsCreateCtrl', function ($scope, $http, Auth, $state) {
     $scope.post = {};
     $scope.create = function() {
-      var error = 'You must be logged in to do that.';
+      //var error = 'You must be logged in to do that.';
       $scope.post.author = Auth.getCurrentUser()._id;
       $http.post('/api/posts',$scope.post)
         .success(function(post){
-          $state.go('posts.read');
+          $state.go('posts.read',post);
         })
         .error(function(error){
           $scope.error = error;
